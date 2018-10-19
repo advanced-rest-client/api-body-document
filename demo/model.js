aaaae1869e586503b2baf4bdff24e2dadbca203e
Pawel Psztyc <jarrodek@gmail.com>
2018-10-18 18:23:38 -0700

    Update: Adding array example computation

--- demo/model.js
+++ demo/model.js
@@ -3,6 +3,7 @@ const generator = require('@api-components/api-model-generator');
 const files = new Map();
 files.set('demo-api/demo-api.raml', 'RAML 1.0');
 files.set('caro-api/caro-api.raml', 'RAML 1.0');
+files.set('array-example/array-example.raml', 'RAML 1.0');
 
 generator(files)
 .then(() => console.log('Finito'));
