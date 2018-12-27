e7b1d8a55402fadfa7b801e1939587f500fa0850
Pawel Psztyc <jarrodek@gmail.com>
2018-12-27 03:04:09 -0800

    Redesigning component to properly (?) use AMF model with examples

--- demo/model.js
+++ demo/model.js
@@ -5,6 +5,7 @@ files.set('demo-api/demo-api.raml', 'RAML 1.0');
 files.set('caro-api/caro-api.raml', 'RAML 1.0');
 files.set('array-example/array-example.raml', 'RAML 1.0');
 files.set('data-types-union/data-types-union.raml', 'RAML 1.0');
+files.set('raml-types/raml-types.raml', 'RAML 1.0');
 
 generator(files)
 .then(() => console.log('Finito'));
