8e35012180dc4dafcc4d89d8d7d79dea163f5e4a
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-12-30 17:54:06 -0300

    Fix anyOf body payload (#15)
    * fix: is payload contains or, treat as object
    
    * build: add prepare script to generate models
    
    * chore(git): ignore model jsons
    
    * chore(demo): delete model jsons, add anyOf api
    
    * chore(deps): regenerate package-lock.json
    
    * test: add test for anyOf payload
    
    * build: remove travis, add github action
    
    * test: migrate test runner
    
    * chore: remove codemirror import
    
    * 4.2.1

---
+++ web-test-runner.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  files: 'test/**/*.test.js',
+  nodeResolve: true,
+  middleware: [
+    function rewriteBase(context, next) {
+      if (context.url.indexOf('/base') === 0) {
+        context.url = context.url.replace('/base', '');
+      }
+      return next();
+    }
+  ],
+  testRunnerHtml: (testFramework) =>
+    `<html>
+		<body>
+		  <script type="module" src="${testFramework}"></script>
+		</body>
+	  </html>`
+};
